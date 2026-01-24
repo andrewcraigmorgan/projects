08c5ee3709d2be5caf3845078d577d447b9f61d3
Andrew Morgan <andrew.morgan@mtc.co.uk>
2026-01-24 22:11:43 +0000

    Add cache-busting param to favicon URLs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,8 @@ export default defineNuxtConfig({
     head: {
       title: 'Projects',
       link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg?v=2' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=2' },
       ],
       script: [
         {
